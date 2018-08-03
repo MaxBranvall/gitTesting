9db33fb1b35ae5dc505ae6baded0265c46bf4121
Max Branvall <maxbranvall@gmail.com>
2018-08-03 02:12:51 -0500

    space

--- testFile3.py
+++ testFile3.py
@@ -4,8 +4,5 @@ print('change')
 print('another change')
 print('no')
 
-<<<<<<< HEAD:testFile3.py
 print('change made in master branch')
-=======
 print('this is in the testing branch')
->>>>>>> testing:testfile3.py
